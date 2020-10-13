23a94aa87ff91b6bddccf10e59f96d9131a0160c
yamahare <jidai.norio@gmail.com>
2020-10-13 19:00:21 +0900

    [add]Buttonコンポーネント追加。module.css追加

---
+++ components/Button.js
@@ -0,0 +1,11 @@
+import style from './Button.module.css'
+
+export default function Button(){
+  return (
+    <button
+      type="button"
+      className={style.error}
+    >Destroy
+    </button>
+  )
+}
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,12 @@
+import Button from './../components/Button'
+
 function HomePage(){
-  return <div>aboutだよおおおおおお</div>
+  return (
+    <>
+      <div>aboutだよおおおおおお</div>
+      <Button></Button>
+    </>
+  )
 }
 export default HomePage
 
