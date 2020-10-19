7c5711d08e1f47b17a5c34a54eb9de25bb85e005
yamahare <jidai.norio@gmail.com>
2020-10-19 21:22:23 +0900

    [add]tailwind.config.js修正２

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
+    removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
+  },
+  purge: {
+    layers: ['utilities'],
+    content: [
+      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}'
+    ]
   },
   theme: {
     extend: {},
