6972c395f0ef1a20d27c97b9681b20412f1395c4
yamahare <jidai.norio@gmail.com>
2020-10-20 19:19:20 +0900

    [add]途中だけどえいや

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
 import Layout from './../components/Layout'
+import { useEffect, useState } from 'react'
 
 function HomePage(){
+  const [data, setData] = useState([])
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch('/api')
+      const newData = await res.json()
+      setData(newData)
+    }
+    getData()
+  }, [])
+
   return (
     <Layout>
       <div>ホームだよ(´ . .̫ . `)</div>
+      {data.length > 0 ? (
+        data.map(d => (
+          <TableRow
+            key={d.data.telephone}
+            creditCard={d.data.creditCard.number}
+            firstName={d.data.firstName}
+            lastName={d.data.lastName}
+            telephone={d.data.telephone}
+          />
+        ))
+      ) : (
+        <>
+        </>
+      )}
     </Layout>
   )
 }
